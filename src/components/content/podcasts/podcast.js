4fb15b5b3d4eddb37cada47bf74c9b7c4969389f
Nissim Ram <nissimram1812@gmail.com>
2021-09-23 14:31:44 -0400

    added social media btns to podcast

--- src/components/content/podcasts/podcast.js
+++ src/components/content/podcasts/podcast.js
@@ -6,6 +6,13 @@ const Podcast = () => {
     return (
         <div>
             <img src = {PodcastImg} alt = "Corner Convos and Chips Logo" className = "convos-logo"></img>
+                <div className="socials-wrapper"> 
+                    <img src="https://www.transparentpng.com/thumb/logo-instagram/JFyofc-logo-instagram-background-png.png" 
+                     alt="Link to instagram" className = "ig-button"/>
+
+                    <img src="https://www.transparentpng.com/thumb/youtube-logo/transparent-youtube-logo-clipart-6.png"
+                    alt="Transparent Youtube Logo Clipart @transparentpng.com" className = "youtube-btn"/>
+                </div> 
             <h3 className = "podcast-gen-description"> A cutting-edge football podcast where we discuss Europe's top five leagues</h3>
            
             <PodcastEpisode title = "All About Arsenal" embedurl = "https://www.youtube.com/embed/CM4WnwdNPK4"/>
